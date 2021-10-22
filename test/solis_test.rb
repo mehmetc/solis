6d62ab6f38ab2df4e42b41f3b9bd61b939f6cd4f
Mehmet Celik <mehmetc@celik.be>
2021-10-23 01:19:20 +0200

    added extra tests
    Took 37 minutes

--- test/solis_test.rb
+++ test/solis_test.rb
@@ -11,6 +11,36 @@ class SolisTest < Minitest::Test
     assert_includes(@solis.list_shapes, 'Course', 'Course not found in shape list')
   end
 
+  def test_should_have_a_model_template_with_attributes
+    course = @solis.shape_as_model('Course')
+    template = course.model_template
+
+    assert_equal(template[:type], 'courses')
+    assert_includes(template, :attributes)
+    assert_includes(template[:attributes], :name)
+  end
+
+  def test_should_have_setup_a_course_model
+    course = @solis.shape_as_model('Course')
+    assert_same(course, Course)
+  end
+
+  def test_create_an_instance
+    course_name='Algebra 101'
+    course = Course.new({name: course_name})
+
+    assert_kind_of(Course, course)
+    assert_equal(course.name, course_name)
+  end
+
+  def test_course_returns_ttl
+    course_name='Algebra 101'
+    course = Course.new({id:1, name: course_name})
+    file_course = File.read('./test/resources/course_1.ttl')
+
+    assert_match(file_course, course.to_ttl)
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Solis::VERSION
   end
