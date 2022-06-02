2bb0480cdfd60efb1ab9944713a93461c9f1ba41
Mehmet Celik <mehmet@celik.be>
2022-06-02 10:19:39 +0200

    datatype test

---
+++ test/datatype_test.rb
@@ -0,0 +1,32 @@
+require "test_helper"
+
+class DatatypeTest < Minitest::Test
+  def setup
+    @solis = Solis::Graph.new(Solis::Shape::Reader::File.read(Solis::ConfigFile[:solis][:shacl]), Solis::ConfigFile[:solis][:env])
+  end
+
+  def test_lang_string
+    @solis.flush_all('http://solis.template/')
+    e = EveryDataType.new({id: '1', lang_string_dt: 'Hello world'})
+    e.save
+
+    r = EveryDataTypeResource.all({filter:{id: '1'}})
+
+    a = r.data.first.lang_string_dt
+
+    assert_equal(["@language", "@value"], a.keys.sort)
+
+    e = EveryDataType.new({id: '2', lang_string_array_dt: ['Hello world', 'Bonjour monde']})
+    e.save
+
+    r = EveryDataTypeResource.all({filter: {id: '2'}})
+
+    a = r.data.first.lang_string_array_dt
+
+    assert_equal(["@language", "@value"], a.keys.sort)
+    assert_kind_of(Array, a["@value"])
+
+    puts r.to_jsonapi
+  end
+
+end
