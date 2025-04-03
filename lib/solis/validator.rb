f185e77bd8a3ef9c3aec92b75b05ac49d01a3273
Davide Monari <davide.monari@kuleuven.be>
2025-04-03 11:20:52 +0200

    feat(validator): added shacl validator

---
+++ lib/solis/validator.rb
@@ -0,0 +1,35 @@
+
+require 'linkeddata'
+require 'shacl'
+require 'json/ld'
+
+module Solis
+  class SHACLValidator
+
+    def initialize(shacl, format)
+      if format.eql?(:ttl)
+        graph_shacl = RDF::Graph.new
+        graph_shacl.from_ttl(shacl)
+      elsif format.eql?(:graph)
+        graph_shacl = shacl
+      end
+      @graph_shacl = graph_shacl
+      @validator = SHACL.get_shapes(@graph_shacl)
+    end
+
+    def execute(data, format)
+      if format.eql?(:jsonld)
+        graph_data = RDF::Graph.new << JSON::LD::API.toRdf(data)
+      elsif format.eql?(:graph)
+        graph_data = data
+      end
+      report = @validator.execute(graph_data)
+      errors = report.results
+      messages = errors.map do |e|
+        "#{e.focus}, #{e.path}: #{e.message}"
+      end
+      [report.conform?, messages]
+    end
+
+  end
+end
