5a36180d11d8705fed3116b8f8fe581fa30702b9
Davide Monari <davide.monari@kuleuven.be>
2025-04-18 16:18:47 +0200

    feat(jsonld): added more utils

--- lib/solis/utils/jsonld.rb
+++ lib/solis/utils/jsonld.rb
@@ -2,6 +2,9 @@
 require 'linkeddata'
 require 'json/ld'
 require 'tsort'
+require 'uri'
+require 'securerandom'
+
 require_relative '../model/parser/shacl'
 
 
@@ -18,6 +21,28 @@ module Solis
   module Utils
     module JSONLD
 
+      def self.expand(obj)
+        arr = JSON::LD::API.expand(obj, options: {
+        })
+        # The library method above seems to provide the same "@type"
+        # "http://www.w3.org/2001/XMLSchema#string" to everything,
+        # unless "@type" was alreadty specified externally.
+        # For now, whden the type is like that, it is deleted.
+        # For the validation, the type is let be inferred by the SHACL validator.
+        arr.each do |obj|
+          obj.each do |name_attr, val_attr|
+            next if ['@id', '@type'].include?(name_attr)
+            val_attr.each do |spec|
+              if spec.key?('@value')
+                if spec.key?('@type') and spec['@type'].eql?('http://www.w3.org/2001/XMLSchema#string')
+                  spec.delete('@type')
+                end
+              end
+            end
+          end
+        end
+        arr
+      end
       def self.flatten_jsonld(hash_jsonld)
         flattened = JSON::LD::API.flatten(hash_jsonld, hash_jsonld['@context'], options: {
           # add flattening algo options here if necessary
@@ -74,7 +99,7 @@ module Solis
           next if ['@id', '@type'].include?(name_attr)
           val_attr = [val_attr] unless val_attr.is_a?(Array)
           val_attr.each do |e|
-            if e.is_a?(Hash)
+            if is_object_an_embedded_entity_or_ref(e)
               type_embedded = SHACLSHapes::get_property_class_for_shape(shapes, type_root, name_attr)
               infer_jsonld_types_from_shapes!(e, shapes, type_embedded)
             end
@@ -94,6 +119,34 @@ module Solis
           } unless datatype.nil?
         end
         context
+      end
+
+      def self.clean_flattened_expanded_from_unset_data!(flattened_expanded)
+        flattened_expanded.each do |obj|
+          obj.each do |name_attr, content_attr|
+            next if ['@id', '@type'].include?(name_attr)
+            content_attr.map! do |content|
+              (content.key?('@value') and content['@value'].eql?('@unset')) ? nil : content
+            end
+            content_attr.compact!
+          end
+        end
+      end
+
+      def self.is_object_an_embedded_entity_or_ref(obj)
+        obj.is_a?(Hash) and (obj.key?('@id') or (!obj.key?('@value') and !obj.key?('@list') and !obj.key?('@set')))
+      end
+
+      def self.add_ids_if_not_exists!(obj, namespace)
+        obj['@id'] = obj['@id'] || URI.join(namespace, SecureRandom.uuid).to_s
+        obj.each do |name_attr, val_attr|
+          val_attr = [val_attr] unless val_attr.is_a?(Array)
+          val_attr.each do |e|
+            if e.is_a?(Hash) and !e.key?('@value')
+              add_ids_if_not_exists!(e, namespace)
+            end
+          end
+        end
       end
 
     end
