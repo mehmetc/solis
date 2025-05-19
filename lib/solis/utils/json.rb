ab7131761e175321c1053892f000c8aeeb7c8383
Davide Monari <davide.monari@kuleuven.be>
2025-05-19 15:10:52 +0200

    fix(json): fixed implementation

--- lib/solis/utils/json.rb
+++ lib/solis/utils/json.rb
@@ -10,12 +10,16 @@ module Solis
           if name_attr.start_with?(prefix_old)
             name_attr_new = name_attr.sub(prefix_old, prefix_new)
             obj2.transform_keys!({name_attr => name_attr_new})
+            name_attr = name_attr_new
           end
           val_attr = obj2[name_attr]
-          val_attr = [val_attr] unless val_attr.is_a?(Array)
-          val_attr.each do |e|
-            if e.is_a?(Hash)
-              obj2[name_attr] = deep_replace_prefix_in_name_attr(e, prefix_old, prefix_new)
+          if val_attr.is_a?(Hash)
+            obj2[name_attr] = deep_replace_prefix_in_name_attr(val_attr, prefix_old, prefix_new)
+          elsif val_attr.is_a?(Array)
+            val_attr.each_with_index do |e, i|
+              if e.is_a?(Hash)
+                obj2[name_attr][i] = deep_replace_prefix_in_name_attr(e, prefix_old, prefix_new)
+              end
             end
           end
         end
